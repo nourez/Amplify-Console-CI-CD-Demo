69032477634e6390aa18ddccff4139bb794cf8f4
nourez <nourez@nourez.dev>
2021-08-02 11:29:49 -0400

    updated text on homepage

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Scroll down to sign out!</h1>
       </header>
       <AmplifySignOut />
     </div>
